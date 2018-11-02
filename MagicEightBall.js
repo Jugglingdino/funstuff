5046de319aacc9057db625925bec96bad453770c
aqf <alexqfredrickson@gmail.com>
2018-11-01 21:35:43 -0400

    created HTML file

---
+++ MagicEightBall.js
@@ -0,0 +1,14 @@
+var string = "sdhfodsjfojoijeoia";
+var num_1 = 1234567; 
+var num_2 = 3949532.32524;
+var true_or_false = false;
+var this_thing_is_null = null;
+var this_is_undefined = undefined;
+
+function Person() {
+	this.name = "Johnathan";
+};
+
+var johnathan = new Person();
+
+console.log(johnathan.name);
